56ebc1815875db5f18ada2320ba6fd59f6118590
Chaynor Hsiao <chaynsaw@gmail.com>
2021-04-05 20:32:41 -0700

    Change 'mom' to whole alphabet

--- wordBank.js
+++ wordBank.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const wordBank = ['mom', 'dad', 'brother'];
+const wordBank = ['abcdefghijklmnopqrstuvwxyz', 'dad', 'brother'];
 
 const mySection = document.getElementById('word-bank');
-let currentWord = wordBank[0]; // test for mom
+let currentWord = wordBank[0]; // test for whole alphabet
 let currentWordWithSpan = '';
 for (let i in currentWord) {
   currentWordWithSpan += `<span id="${i}">${currentWord[i]}</span>`;
