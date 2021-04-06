c38d47032820dfd2062e0b2831c8e2ca3b9d0e4f
Piao <wenhaopi@08f8bc6186e1.ant.amazon.com>
2021-04-06 12:41:42 -0700

    Add user input box and implement its functionality

--- wordBank.js
+++ wordBank.js
@@ -1,17 +1,19 @@
 "use strict";
 
 const wordBank = ["abcdefghijklmnopqrstuvwxyz", "dad", "brother"];
-
 const mySection = document.getElementById("word-bank");
-let currentWord = wordBank[0]; // test for whole alphabet
-let currentWordWithSpan = "";
-for (let i in currentWord) {
-  currentWordWithSpan += `<span id="${i}">${currentWord[i]}</span>`;
-}
 
-mySection.innerHTML = currentWordWithSpan;
+const renderWord = () => {
+  let currentWord = wordBank[0]; // test for whole alphabet
+  let currentWordWithSpan = "";
+  for (let i in currentWord) {
+    currentWordWithSpan += `<span id="${i}">${currentWord[i]}</span>`;
+  }
+  mySection.innerHTML = currentWordWithSpan;
+};
 
 const isValidWordBankLetter = (letter) => {
+  let currentWord = wordBank[0];
   for (let i in currentWord) {
     if (currentWord[i] === letter) {
       document.getElementById(`${i}`).style.color = "green";
@@ -20,5 +22,15 @@ const isValidWordBankLetter = (letter) => {
   }
   return false;
 };
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  wordBank.unshift(e.target.word.value);
+  renderWord();
+};
+
+renderWord();
+
+document.getElementById("add-word").addEventListener("submit", handleSubmit);
 
 // letterValidation('o'); // use this in the event handler
