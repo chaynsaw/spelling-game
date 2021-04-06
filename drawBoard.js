9ea0efea74499f00020207235ba35a6abb22fb94
Chaynor Hsiao <chaynsaw@gmail.com>
2021-04-06 14:12:54 -0700

    Prettier reformat

--- drawBoard.js
+++ drawBoard.js
@@ -1,22 +1,19 @@
 const drawBoard = () => {
   const canvas = document.getElementById("board");
   const ctx = canvas.getContext("2d");
-  
+
   let y = 40;
 
   const draw = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      if (y < canvas.height) {
-        y += 1;
-      } else {
-        y = 40;
-      }
-      drawLetters(12, y)
-      console.log(y);
-      window.requestAnimationFrame(draw)
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (y < canvas.height) {
+      y += 1;
+    } else {
+      y = 40;
     }
-
+    drawLetters(12, y);
+    console.log(y);
+    window.requestAnimationFrame(draw);
+  };
   window.requestAnimationFrame(draw);
-  
-
-}
+};
